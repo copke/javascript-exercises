849429a2982620840c281ea3b0bcb367fd299681
copke <117200119+copke@users.noreply.github.com>
2024-10-02 22:07:53 +0200

    Write a function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatWord = "";
+    if(num < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++) {
+        repeatWord += string;
+    }
 
+    return repeatWord;
 };
 
 // Do not edit below this line
