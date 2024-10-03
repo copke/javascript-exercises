bbb6eae8f908706503450315a164ac3b31790ffd
copke <117200119+copke@users.noreply.github.com>
2024-10-03 12:41:54 +0200

    Write a function that reverses a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const array = string.split("");
 
+    let newArray = [];
+
+    for(let i = array.length - 1; i >= 0; i--) {
+        newArray.push(array[i]);
+        
+    }
+    
+    let reversedString = newArray.join("");
+    
+    return reversedString;
 };
 
 // Do not edit below this line
