cbed1cddd9801c95bd1e8eb42001c42b85cd5b95
copke <117200119+copke@users.noreply.github.com>
2024-10-07 17:34:37 +0200

    Write a function that takes 2 positive integers and returns the sum of every integer between (and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let total = 0;
+    if(n1 < 0 || n2 < 0) {
+       return "ERROR";
+    }
+    else if(!Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
+    else if(n1 < n2) {
+        for(let i = n1; i <= n2; i++) {
+            total += i;
+        }
+        return total;
+    }
+    else {
+        for(let i = n1; i >= n2; i--) {
+            total += i;
+        }
+        return total;
+    }   
 };
 
 // Do not edit below this line
